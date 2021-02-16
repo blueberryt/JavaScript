3a40a80e7ab99a1bced507bc7594bee54ef5d01c
Risa Harada <supikateriburu@gmail.com>
2021-02-17 08:54:32 +0900

    :page_with_curl: Add 3-2-1

---
+++ 実践TypeScript/dist/3-2-1.js
@@ -0,0 +1,4 @@
+"use strict";
+var a1 = [true, false];
+var a2 = [0, 1, "2"];
+var a3 = [false, 1, "2"];
---
+++ 実践TypeScript/src/3-2-1.ts
@@ -0,0 +1,3 @@
+const a1 = [true, false]
+const a2 = [0, 1, "2"]
+const a3 = [false, 1, "2"]
