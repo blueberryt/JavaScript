36804e8a4010d7c4d13ce852e92a76475e32a1a3
Risa Harada <supikateriburu@gmail.com>
2021-02-17 08:50:48 +0900

    :page_with_curl: Add 3-1-5

---
+++ 実践TypeScript/dist/3-1-5.js
@@ -0,0 +1,14 @@
+"use strict";
+var wideningZero = 0;
+var nonWideningZero = 0;
+var asNonWideningZero = 0;
+var zeroA = 0;
+var zeroB = wideningZero;
+var zeroC = nonWideningZero;
+var zeroD = asNonWideningZero;
+var zeros = {
+    zeroA: zeroA,
+    zeroB: zeroB,
+    zeroC: zeroC,
+    zeroD: zeroD,
+};
---
+++ 実践TypeScript/src/3-1-5.ts
@@ -0,0 +1,15 @@
+const wideningZero = 0
+const nonWideningZero: 0 = 0
+const asNonWideningZero = 0 as 0
+
+let zeroA = 0
+let zeroB = wideningZero
+let zeroC = nonWideningZero
+let zeroD = asNonWideningZero
+
+const zeros = {
+  zeroA,
+  zeroB,
+  zeroC,
+  zeroD,
+}
