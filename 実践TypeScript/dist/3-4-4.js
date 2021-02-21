dcabe61e76e4041291497e3ecccb391829215ff3
Risa Harada <supikateriburu@gmail.com>
2021-02-22 08:58:39 +0900

    :page_with_curl: Add 3-4-4

---
+++ 実践TypeScript/dist/3-4-4.js
@@ -0,0 +1,6 @@
+"use strict";
+function getScore(score) {
+    if (score < 0 || score > 100)
+        return null;
+    return score;
+}
---
+++ 実践TypeScript/src/3-4-4.ts
@@ -0,0 +1,4 @@
+function getScore(score: number) {
+  if (score < 0 || score > 100) return null
+  return score
+}
