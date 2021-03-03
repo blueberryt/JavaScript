09b6c337281803707aa6585c38f45fb1f8df96d9
Risa Harada <supikateriburu@gmail.com>
2021-03-03 10:33:00 +0900

    :page_with_curl: Add 4-1-1 with warning

---
+++ 実践TypeScript/src/4-1/4-1-1.ts
@@ -0,0 +1,7 @@
+function getFormattedValue(value: number | null ) {
+  return `${value.toFixed(1)} pt`
+}
+
+console.log(getFormattedValue(0.1))
+console.log(getFormattedValue(0))
+console.log(getFormattedValue(null))
