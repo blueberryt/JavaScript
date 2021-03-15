9f29aa6b5b073a33431522d24053ca838d5dda2b
Risa Harada <supikateriburu@gmail.com>
2021-03-16 08:43:16 +0900

    :page_with_curl: Add 4-2-21const assertion

---
+++ 実践TypeScript/dist/4-2/4-2-21/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var constants_1 = __importDefault(require("./constants"));
+var n = constants_1.default;
